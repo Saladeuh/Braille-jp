42f9bc8f01a8066a925e1f86f220f030fe0044f8
Saladeuh <emilienmonet10@gmail.com>
2025-04-18 10:54:03 +0200

    add french table in supported tables

--- BrailleJP/Game1.SupportedBrailleTables.cs
+++ BrailleJP/Game1.SupportedBrailleTables.cs
@@ -8,6 +8,7 @@ public partial class Game1
 {
   public static readonly Dictionary<CultureInfo, string> SUPPORTEDBRAILLETABLES = new Dictionary<CultureInfo, string>
   {
-    {new CultureInfo("ja-Jp"), "ja-jp-comp6" }
+    {new CultureInfo("ja-Jp"), "ja-jp-comp6" },
+    {new CultureInfo("fr-FR"), "fr-bfu-comp8" }
   };
 }
